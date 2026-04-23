4499ca03d06a0b7eb27165094a6b55f1df1a61db
Mason Winsauer <mason.winsauer@gmail.com>
2026-04-23 13:09:21 -0400

    Quartz sync: Apr 23, 2026, 1:09 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       "Also Me \:\)": "https://masonwinsauer.com",
-      "":"These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental."
+      "These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental.":""
     },
   }),
 }
