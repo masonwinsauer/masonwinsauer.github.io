583a9dd6d108c0e112a81f0433da124debbe16b0
Mason Winsauer <mason.winsauer@gmail.com>
2026-04-23 20:38:32 -0400

    Quartz sync: Apr 23, 2026, 8:38 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} <br/>
-          "These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental."<br/>
+          <i>"These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental." </i><br/>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
