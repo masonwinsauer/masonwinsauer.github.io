a57187cedef1e50f26b1813b260627c07c6bc3d8
Mason Winsauer <mason.winsauer@gmail.com>
2026-04-23 20:31:22 -0400

    Quartz sync: Apr 23, 2026, 8:31 PM

--- content/Untitled Sketches/index.md
+++ content/Untitled Sketches/index.md
@@ -5,5 +5,3 @@ description:
 [A collection of unedited entries by Mason Winsauer.]
 
 Some use charcoal, oil, marker.
-
-* These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental.*
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,9 +14,9 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <span class="italic">"These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental." <span/> <br>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} <br/>
+          "These are works of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental."<br/>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
